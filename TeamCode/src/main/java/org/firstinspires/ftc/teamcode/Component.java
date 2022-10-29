0264b154f19ff49c7ac068a45d05aad67eababaf
srcardosi <srcardosi@icloud.com>
2022-10-29 12:27:46 -0400

    Hopeful TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Component.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+/**
+ * Created by Sean Cardosi on 10/28/22.
+ */
+public class Component {
+
+    public DcMotor lift;
+    public DcMotor arm;
+    public Servo leftGrabber;
+    public Servo rightGrabber;
+
+    public Component(HardwareMap hardwareMap) {
+
+        lift = hardwareMap.dcMotor.get("lift");
+        lift.setDirection(DcMotor.Direction.FORWARD);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm = hardwareMap.dcMotor.get("arm");
+        arm.setDirection(DcMotor.Direction.FORWARD);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftGrabber = hardwareMap.servo.get("lGrab");
+        leftGrabber.setDirection(Servo.Direction.FORWARD);
+        rightGrabber = hardwareMap.servo.get("rGrab");
+        rightGrabber.setDirection(Servo.Direction.REVERSE);
+    }
+
+    public void moveLift(double power) {
+        lift.setPower(power);
+    }
+
+    public void stopLift() {
+        lift.setPower(0.0);
+    }
+
+    public void grab(){
+        leftGrabber.setPosition(60);
+        rightGrabber.setPosition(60);
+    }
+
+    public void release() {
+        leftGrabber.setPosition(0);
+        rightGrabber.setPosition(0);
+    }
+
+    public void moveArm(double power) {
+        arm.setPower(power);
+    }
+
+    public void stopArm() {
+        arm.setPower(0);
+    }
+
+    public void getTelemetry(Telemetry telemetry) {
+        telemetry.addData("Lift Encoder", lift.getCurrentPosition());
+        telemetry.addData("Arm Encoder", arm.getCurrentPosition());
+        telemetry.addData("Left Grabber Position", leftGrabber.getPosition());
+        telemetry.addData("Right Grabber Position", rightGrabber.getPosition());
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleOp.java
@@ -0,0 +1,114 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Drivetrain;
+
+/**
+ * Created by Sean Cardosi on 8/28/22.
+ */
+@TeleOp(name = "RobotTeleOp", group = "Octavius")
+public class RobotTeleOp extends OpMode {
+
+    private Drivetrain robot;
+    private Component component;
+    private boolean isReady = false;
+    boolean isLiftHigh = false;
+    boolean isLiftUp = false;
+    boolean isLiftDown = true;
+    boolean isGrabberClosed = true;
+    final double MAX_LIFT_ENCODERS = 5000;
+    final double MIN_LIFT_ENCODERS = 200;
+    final double MIN_SWING_ENCODERS = 1000;
+
+    @Override
+    public void init() {
+
+        //Initialize robot
+        robot = new Drivetrain(hardwareMap);
+        robot.runUsingEncoders();
+        component = new Component(hardwareMap);
+        isReady = true;
+        isLiftHigh = false;
+        isLiftUp = false;
+        isLiftDown = true;
+        isGrabberClosed = true;
+    }
+
+    @Override
+    public void init_loop() {
+        if (isReady) {
+            telemetry.addData(">", "Robot Ready!");
+            telemetry.update();
+        }
+    }
+
+    @Override
+    public void start() {
+        super.start();
+    }
+
+    @Override
+    public void loop() {
+        component.getTelemetry(telemetry);
+
+
+        //----------------------------------------------=+(Drivetrain)+=----------------------------------------------\\
+        robot.drive(gamepad1, telemetry);
+
+        if (gamepad1.x) {
+            robot.resetHeading();
+        }
+        //----------------------------------------------=+(Drivetrain)+=----------------------------------------------\\
+
+
+        //----------------------------------------------=+(Grabber)+=----------------------------------------------\\
+        if (gamepad2.left_bumper) {
+            component.grab();
+        } else if (gamepad2.right_bumper) {
+            component.release();
+        }
+        //----------------------------------------------=+(Grabber)+=----------------------------------------------\\
+
+
+        //----------------------------------------------=+(Lift)+=----------------------------------------------\\
+        if (gamepad2.right_stick_y > 0.1 && !isLiftUp) {
+            component.moveLift(1.0);
+            isLiftDown = false;
+        } else if (gamepad2.right_stick_y < -0.1 && !isLiftDown) {
+            component.moveLift(-1.0);
+            isLiftUp = false;
+            isLiftHigh = false;
+        } else {
+            component.stopLift();
+        }
+
+        if (component.lift.getCurrentPosition() >= MAX_LIFT_ENCODERS) {
+            isLiftUp = true;
+            isLiftHigh = true;
+            //TODO: FINISH THIS SO YOU DONT BREAK LIFT
+        } else if (component.lift.getCurrentPosition() <= MIN_LIFT_ENCODERS) {
+            isLiftDown = true;
+            isLiftHigh = false;
+        }
+
+        if (component.lift.getCurrentPosition() >= MIN_SWING_ENCODERS) {
+            isLiftHigh = true;
+        } else {
+            isLiftHigh = false;
+        }
+        //----------------------------------------------=+(Lift)+=----------------------------------------------\\
+
+
+        //----------------------------------------------=+(Arm)+=----------------------------------------------\\
+        if (gamepad2.left_stick_y > 0.1 && isLiftHigh) {
+            component.moveArm(0.6);
+        } else if (gamepad2.left_stick_y < -0.1 && isLiftHigh) {
+            component.moveArm(-0.6);
+        } else {
+            component.stopArm();
+        }
+        //----------------------------------------------=+(Arm)+=----------------------------------------------\\
+    }
+}
