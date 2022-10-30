9159aeb7afe5ab440f9906ada077c117035389cf
srcardosi <srcardosi@icloud.com>
2022-10-30 17:56:41 -0400

    Added LiftTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTest.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by Sean Cardosi on 10/30/22.
+ */
+@TeleOp(name = "LiftTest", group = "Test")
+public class LiftTest extends OpMode {
+
+    DcMotor lift;
+
+    @Override
+    public void init() {
+        lift = hardwareMap.dcMotor.get("lift");
+        lift.setDirection(DcMotor.Direction.FORWARD);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            lift.setPower(0.6);
+        }
+    }
+}
