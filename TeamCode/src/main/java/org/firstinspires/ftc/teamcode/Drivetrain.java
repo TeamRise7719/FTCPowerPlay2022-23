754d3b4810699e23e64ed98ff9cdc0a8941c62ef
srcardosi <srcardosi@icloud.com>
2022-11-04 14:30:58 -0400

    Fix Turning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -148,7 +148,7 @@ public class Drivetrain {
     public void drive(Gamepad gamepad1, Telemetry telemetry) {
         loop();
 
-        final double gx = gamepad1.left_stick_x;
+        final double gx = -gamepad1.left_stick_x;
         final double gy = -gamepad1.left_stick_y;
         final double r = -(gamepad1.right_stick_x);
 //        final double direction = Math.atan2(x, y) + getHeading();
