f3dd0f97e9bcec31fdbc67b1ba68ca852b50d5bd
Beastmodexol <beastmodexol1@gmail.com>
2022-12-02 15:54:29 -0500

    Adding Treading Tests

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/TreadingTesting/AutoTest.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.Test.TreadingTesting;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class AutoTest {
+
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/TreadingTesting/ThreadCommands2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/TreadingTesting/ThreadCommands2.java
@@ -1,25 +1,35 @@
 package org.firstinspires.ftc.teamcode.Test.TreadingTesting;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Sensing.SeansEncLibrary;
+
 
 /**
  * Created by Jeremiah on 11/12/22.
  */
-public class ThreadCommands2 extends Thread{
+public class ThreadCommands2 implements Runnable{
 
     public Servo leftGrabber;
     public Servo rightGrabber;
+    public DcMotor lift;
+    SeansEncLibrary enc;
 
     public ThreadCommands2(HardwareMap hardwareMap){
         leftGrabber = hardwareMap.servo.get("lGrab");
         leftGrabber.setDirection(Servo.Direction.FORWARD);
         rightGrabber = hardwareMap.servo.get("rGrab");
         rightGrabber.setDirection(Servo.Direction.REVERSE);
+        lift = hardwareMap.dcMotor.get("lift");
+        lift.setDirection(DcMotor.Direction.FORWARD);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     public void run() {
-        leftGrabber.setPosition(0.55);
-        rightGrabber.setPosition(1.0);
+        lift.setPower(.4);
+        enc.steeringDrive(10,false,false);
     }
+
 }
