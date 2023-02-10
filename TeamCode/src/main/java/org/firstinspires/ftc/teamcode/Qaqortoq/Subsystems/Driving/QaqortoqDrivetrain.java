e7ddea1c0f94d6e840e35662917be317cbca4180
srcardosi <srcardosi@icloud.com>
2023-02-10 13:41:52 -0500

    Increase amount of allowed tilt to 15º

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qaqortoq/Subsystems/Driving/QaqortoqDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qaqortoq/Subsystems/Driving/QaqortoqDrivetrain.java
@@ -34,7 +34,7 @@ public class QaqortoqDrivetrain {
 
     SeansSynchronousPID tiltPID;
     double tiltP = 0.1;
-    double tiltError = 6;//degrees
+    double tiltError = 15;//degrees
 
     SeansSynchronousPID pid;
 
