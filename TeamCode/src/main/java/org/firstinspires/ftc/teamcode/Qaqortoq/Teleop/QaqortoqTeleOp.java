f69baa8ce45bf92ba468d4a43a192a404fdaea21
Icy Hot <44911827+PhantomofDeath99@users.noreply.github.com>
2023-02-21 17:14:04 -0500

    Arm PID changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qaqortoq/Teleop/QaqortoqTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qaqortoq/Teleop/QaqortoqTeleOp.java
@@ -46,9 +46,9 @@ public class QaqortoqTeleOp extends OpMode {
     List<LynxModule> allHubs;
     double armFFAngle = 135;
     double kcos = -0.1;
-    double armP = 0.9;
+    double armP = 1.5;
     double armI = 0.0;
-    double armD = 0.02;
+    double armD = 0.065;
     AnalogInput pot;
     DcMotorEx armMotor;
     PIDController goingUp;
