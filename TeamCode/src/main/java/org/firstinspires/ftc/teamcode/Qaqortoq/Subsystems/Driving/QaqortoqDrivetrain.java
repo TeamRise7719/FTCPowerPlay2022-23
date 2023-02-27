58a30ec3b3111c68bc7387ba65c82c2c7f14b9aa
srcardosi <srcardosi@icloud.com>
2023-02-26 21:58:37 -0500

    Re-add Anti-Tip

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qaqortoq/Subsystems/Driving/QaqortoqDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qaqortoq/Subsystems/Driving/QaqortoqDrivetrain.java
@@ -34,7 +34,7 @@ public class QaqortoqDrivetrain {
 
     SeansSynchronousPID tiltPID;
     double tiltP = 0.1;
-    double tiltError = 3.5;//degrees
+    double tiltError = 4.5;//degrees
 
     SeansSynchronousPID pid;
 
